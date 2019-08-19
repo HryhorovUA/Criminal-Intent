9adb8c68bdb512cf6d2521ec4faeb2b171a8d314
Artem Hryhorov <artem.hryhorov@nure.ua>
2019-08-19 11:39:49 +0300

    Add ViewPager
    Crate new class CrimePager Adapter instead class CrimeActivity

--- CrimeListFragment.java
+++ CrimeListFragment.java
@@ -62,7 +62,7 @@ public class CrimeListFragment extends Fragment {
 
         @Override
         public void onClick(View view) {
-            Intent intent = CrimeActivity.newIntent(getActivity(), mCrime.getId());
+            Intent intent = CrimePagerActivity.newIntent(getActivity(), mCrime.getId());
             startActivityForResult(intent, REQUEST_CRIME);
         }
 
